9ea443f3a4590b6b1e56ec4a5fd591c5376dbc2b
Shawn Rigdon <shawn.patrick.rigdon@gmail.com>
2025-10-07 12:25:58 -0400

    making early retirement age and full retirement age variables

--- social_security_comparison.py
+++ social_security_comparison.py
@@ -7,8 +7,10 @@ partial_payment = 2831
 full_payment = 4018
 compound_rate = 0.04
 savings_rate = 0.20
+early_retirement_age = 62
+full_retirement_age = 67
 
-age = np.arange(62, 100, 1.0/12, dtype=np.float32)
+age = np.arange(early_retirement_age, 100, 1.0/12, dtype=np.float32)
 
 monthly_compound_rate = (1 + compound_rate) ** (1/12) - 1
 
@@ -28,8 +30,8 @@ def calculate_total_payments(age_arr, start_age, payment, per_payment_compound_r
         total_balance[i] = total_balance[i-1] + cumulative_interest[i] + payment
     return total_balance
 
-partial_total = calculate_total_payments(age, 62, partial_payment, monthly_compound_rate, savings_rate)
-full_total = calculate_total_payments(age, 67, full_payment, monthly_compound_rate, savings_rate)
+partial_total = calculate_total_payments(age, early_retirement_age, partial_payment, monthly_compound_rate, savings_rate)
+full_total = calculate_total_payments(age, full_retirement_age, full_payment, monthly_compound_rate, savings_rate)
 
 plt.plot(age, partial_total, label='Early Retirement', color='blue')
 plt.plot(age, full_total, label='Full Retirement', color='orange')
